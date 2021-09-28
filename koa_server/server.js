8016d79955f57f0be8c8754a6dec79d889af16d3
zxlalalala <zx2273284193@dingtalk.com>
2021-09-28 17:58:54 +0800

    add websocketServer

--- koa_server/server.js
+++ koa_server/server.js
@@ -11,3 +11,8 @@ const resDataMiddleware = require('./middleware/koa_response_data')
 app.use(resDataMiddleware)
 
 app.listen(8081)
+
+const WebSocketService = require('./service/web_socket_service.js')
+//开启服务器的监听，监听客户端的连接
+//当某一个客户端连接成功之后，就会对这个客户端进行message事件的监听
+WebSocketService.listen()
---
+++ koa_server/service/web_socket_service.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const fileUtils = require('../utils/file_utils.js') //读取文件
+//安装并引入ws包
+const WebSocket = require('ws')
+//创建websocket实例对象，绑定端口
+const wss = new WebSocket.Server({
+    port:9998
+})
+
+module.exports.listen = () => {
+     //对客户端的连接事件进行监听
+    //client：代表的是客户端的连接socket对象
+    wss.on('connection',client => {
+        console.log('有客户端连接了')
+        //msg:由客户端发给服务端的数据
+        client.on('message',async msg => {
+            console.log('客户端发送数据给服务端了：'+msg)
+            let payload = JSON.parse(msg)
+            let action = payload.action
+            if(action==='getData'){
+                let filePath = '../data/'+ payload.chartName+'.json'
+                filePath = path.join(__dirname,filePath)
+                let ret = await fileUtils.getFileJsonDate(filePath)
+                payload.data = ret
+                client.send(JSON.stringify(payload))
+            }else{
+                //原封不动的将所接收到的数据发送给每一个处于连接状态的客户端
+                //wss.clients 所以连接的客户端，是一个数组
+                wss.clients.forEach(client => {
+                    client.send(msg)
+                })
+            }
+        })
+    })
+}
